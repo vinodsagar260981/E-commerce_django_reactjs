2fb229eea9aee84e6f0464976a8579b303c648c9
Vinod Sagar <vinodsagar@Vinods-MacBook-Pro.local>
2021-09-10 20:42:55 +0530

    adding django and react

---
+++ backend/base/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    path('users/register/', views.registerUser, name='user_register'),
+
+    path('users/profile/', views.getUserProfile , name='usersprofile'),
+    path('users/', views.getUsers , name='users'),
+
+    path('products/', views.getProducts , name='products'),
+
+    path('products/<str:pk>/', views.getProductOne , name='ProductOne'),
+]
